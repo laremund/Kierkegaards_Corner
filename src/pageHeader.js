c05ab3897da1a8a3dcab1848a66101b542bc467c
laremund <larsgremund@gmail.com>
2024-08-01 13:59:01 -0600

    Initial commit

---
+++ src/pageHeader.js
@@ -0,0 +1,13 @@
+function pageHeader() {
+    return (
+        <div className="pageHeader">
+            <header className="siteTitle">
+                <div>
+                    div
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default pageHeader;
