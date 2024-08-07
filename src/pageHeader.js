fb1f931768130d135dc19ab957cdf3449cda5f59
laremund <larsgremund@gmail.com>
2024-08-06 18:21:57 -0600

    First commit

--- src/pageHeader.js
+++ src/pageHeader.js
@@ -1,13 +1,16 @@
-function pageHeader() {
+
+
+function PageHeader() {
     return (
-        <div className="pageHeader">
-            <header className="siteTitle">
-                <div>
-                    div
-                </div>
-            </header>
+        <div className="app-header-container">
+            <div className="menu-icon-container">
+                {/* menu icon here */}
+            </div>
+            <div className="site-title-container">
+                <h1 className="site-title-text">Kierkegaard's Corner</h1>
+            </div>
         </div>
     )
 }
 
-export default pageHeader;
+export default PageHeader;
