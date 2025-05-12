109c539c6322f58c75ebc9dfd7115976a1680c60
laremund <larsgremund@gmail.com>
2025-05-12 17:39:34 -0600

    fix(index.js): fixed wrong import name

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import HomePageContent from './HomePageContent.jsx';
 import PageFooter from './PageFooter.jsx';
 import PageHeader from './PageHeader.jsx';
-import WaysOfUnderstanding from './WaysOfPageContent.jsx';
+import WaysOfPageContent from './WaysOfPageContent.jsx';
 import WayOfSection from './WayOfSection.jsx'
 
-export { HomePageContent, PageFooter, PageHeader, WaysOfUnderstanding, WayOfSection }
+export { HomePageContent, PageFooter, PageHeader, WaysOfPageContent, WayOfSection }
